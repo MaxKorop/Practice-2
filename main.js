19746b584fb99e8febbddec61f66fc700aa36604
Max Korop <makskorop99@gmail.com>
2023-09-27 01:24:51 +0300

    first commit

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const year = 2023;
 const greeting = (name, birthYear = 2000)=>{
     console.log(`Я ${name}, мені ${year-birthYear} років`)
 }
-greeting("Anna")
+greeting("Max", 2005);
 
 
 /*-----Task 8-----*/
