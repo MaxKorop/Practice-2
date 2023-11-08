b5f6fa025b7ca79b82009634d16b4d3138fe1570
Max Korop <makskorop99@gmail.com>
2023-11-08 11:56:10 +0200

    Created UI and configured webpack loaders

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 import searchPeople from "./searchPeople.js";
 import people from "./array.js";
+import "./style.css";
+import "./index.html";
 
-const year = 2023;
-const greeting = (name, birthYear = 2000)=> `Я ${name}, мені ${year-birthYear} років`
+const displayGreeting = () => {
+    const name = document.getElementById("name").value;
+    const year = document.getElementById("year").value;
+    const currentYear = new Date().getFullYear();
 
-greeting("Max", 2005);
+    if (name && !isNaN(parseInt(year)) && year > 0 && year <= currentYear) {
+        document.getElementById("greeting").textContent = "Привіт мене звати " + name + ", мені " + (currentYear-year) + " років!";
+    } else {
+        document.getElementById("greeting").textContent = "Дані введено неправильно. Заповніть обидва поля";
+    }
+}
+document.getElementById("displayGreeting").onclick = () => displayGreeting()
 
 
-/*-----Task 8-----*/
+const searchPeopleOnPage = () =>{
+    const searchField = document.getElementById("searchField").value;
+    const searchValue = document.getElementById("searchValue").value;
+    const searchResultElement = document.getElementById("searchResult");
+
+    if (searchField && searchValue) {
+        searchPeople(people, searchField, searchValue)
+            .then((results) => {searchResultElement.innerHTML = `Результати пошуку:<br>${results.map((person) => JSON.stringify(person)).join(" ")}`;})
+            .catch((error) => {searchResultElement.textContent = `Помилка: ${error}`;});
+    } else {
+        searchResultElement.textContent = "Заповніть обидва поля";
+    }
+}
+document.getElementById("search").onclick = () => searchPeopleOnPage()
+
+
+
+
+
+/*=====Task 8=====*/
 searchPeople(people, 'name', 'Person 5')
     .then(result => {console.log("Результати пошуку:", result); return result})
     .then(result => result.forEach(element => {
@@ -23,3 +52,5 @@ searchPeople(people, 'weight', 55)
     .then(result => result.forEach(element => {
         console.log(element.greeting(), element.getInfo('gender'))}))
     .catch(error => console.error("Помилка:", error));
+
+console.log()
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,41 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
     entry: "./index.js",
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.[hash].js',
+        clean: true,
     },
+    module:{
+        rules:[
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.svg$/, 
+                use: 'svg-inline-loader'
+            },
+            { 
+                test: /\.css$/, 
+                use: [ 'style-loader', 'css-loader' ] 
+            },
+            { 
+                test: /\.(js)$/, 
+                use: 'babel-loader' 
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve("index.html")
+        }),
+        new webpack.EnvironmentPlugin({
+            'NODE_ENV': 'production'
+        })      
+    ],
     mode: "production"
 };
